8c8316805adefc577304cffc54694178978047e3
Kevin Isom <kevin.isom@gmail.com>
2014-07-08 22:13:34 +1200

    animated the background and moved ships

--- public/js/game/create.js
+++ public/js/game/create.js
@@ -1,8 +1,12 @@
 module.exports =  function () {
  
     console.log(this);
-    this.add.sprite(0,0,'bg');
-    this.add.sprite(0, 0, 'enemyFighter');
+
+
+    this.spaceBG =  this.add.tileSprite(0, 0, 800, 600, 'bg');  
+    this.spaceBG.autoScroll(0, 75); 
+
+    this.add.sprite((400 - 16), 10, 'enemyFighter');
     this.add.sprite((400 - 16), 500, 'ship');
- 
+
 }
