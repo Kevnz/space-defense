7ace3de244fa6afe8f1a04b94f5631ca4d39f34c
Kevin Isom <kevin.isom@gmail.com>
2014-07-11 07:44:30 +1200

    added half ass call to create laser blast - still need to rotate

--- public/js/game/create.js
+++ public/js/game/create.js
@@ -8,5 +8,5 @@ module.exports =  function () {
 
     this.add.sprite((400 - 16), 10, 'enemyFighter');
     this.add.sprite((400 - 16), 500, 'ship');
-
+    //var this.lazer = this.game.add.tileSprite((400 - 16), 495, width, height, 'bullet');  
 }
