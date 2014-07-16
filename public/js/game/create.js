4b5a59cc69527a18657f150c4eb7037a0af57473
Kevin Isom <kevin.isom@gmail.com>
2014-07-16 19:31:47 +1200

    stuff

--- public/js/game/create.js
+++ public/js/game/create.js
@@ -6,15 +6,17 @@
 ;
 module.exports =    function(){
     console.log(this);
-
+game.physics.startSystem(Phaser.Physics.ARCADE);
 
     this.spaceBG =  this.add.tileSprite(0, 0, 800, 600, 'bg');  
     this.spaceBG.autoScroll(0, 75); 
 
     var ef = window.ef = this.add.sprite((400 - 16), 10, 'atlas');
     ef.frameName = "bad-guy1.png";
+    console.log(ef);
+    game.physics.enable(ef, Phaser.Physics.ARCADE);
     var player = window.player = this.add.sprite((400 - 16), 500, 'ship');
-
+    game.physics.enable(player, Phaser.Physics.ARCADE);
     var cursors = window.cursors = this.input.keyboard.createCursorKeys();
 
     var bullets = window.bullets = this.add.group();
--- public/js/game/preload.js
+++ public/js/game/preload.js
@@ -2,6 +2,7 @@ module.exports =  function () {
  
     //this.load.image('enemyFighter', 'img/enemy-fighters/bad-guy1-a.png');
     this.load.atlas('bullets', 'img/assets/bullets.png', 'img/assets/bullets.json');
+    this.load.image('blue_bullet', 'img/assets/blue-bullet.png');
     this.load.image('bg', 'img/backgrounds/bg.png');
     this.load.image('ship', 'img/space_ship_base.png');
     this.load.atlas('atlas', 'img/enemy-fighters/enemy-fighters.png', 'img/enemy-fighters/enemy-fighters.json');
